39157a23c7a2ae8ef65fa0700647d888a2e06bbe
Abhinav Kumar <abhinavkumar93043@example.com>
2024-09-07 01:54:49 +0530

    adding the link to headings and directing to element by these links

--- clients/src/components/BlogContents/index.jsx
+++ clients/src/components/BlogContents/index.jsx
@@ -1,6 +1,7 @@
-import react, { useEffect } from "react";
-
+import { useEffect, useState } from "react";
+import { toast } from 'sonner'
 const BlogContents = ({ content, className }) => {
+    const [isContentLoaded, setIsContentLoaded] = useState(false);
     const addStylesCopyBtn = () => {
         let snippets = document.getElementsByClassName('ql-syntax');
         let numberOfSnippets = snippets.length;
@@ -43,9 +44,59 @@ const BlogContents = ({ content, className }) => {
         }
     };
 
+    const addLinkToHeadings = () => {
+        document.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((heading) => {
+            if (!heading.id) {
+                return;
+            }
+            heading.classList.add("relative", "group")
+
+            const linkIcon = document.createElement('span');
+            linkIcon.innerHTML = '🔗';
+            linkIcon.className = "w-7 cursor-pointer font-thin dark:text-slate-400 text-slate-800 absolute -left-5 text-base hidden group-hover:block top-1"
+
+            heading.appendChild(linkIcon);
+
+            linkIcon.addEventListener('click', () => {
+                const url = `${window.location.origin}${window.location.pathname}#${heading.id}`;
+                navigator.clipboard.writeText(url).then(() => {
+                    toast('Link copied to clipboard', { position: "top-center" });
+                }).catch((err) => {
+                    console.error('Failed to copy: ', err);
+                });
+            });
+        });
+    }
+
+    const scrollToElement = (element) => {
+        const elementPosition = element.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.scrollY - 80;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth',
+        });
+    };
+
     useEffect(() => {
+        if (isContentLoaded) {
+            const hash = window.location.hash.substring(1);
+            if (hash) {
+                const element = document.getElementById(hash);
+                if (element) {
+                    scrollToElement(element);
+                }
+            }
+        }
+    }, [isContentLoaded]);
+
+    useEffect(() => {
+        if (content) {
+            setIsContentLoaded(true);
+        }
         addStylesCopyBtn();
-    }, []);
+        addLinkToHeadings();
+    }, [content]);
 
     return (
         <div className={`content ${className}`} dangerouslySetInnerHTML={{ __html: content }} />
