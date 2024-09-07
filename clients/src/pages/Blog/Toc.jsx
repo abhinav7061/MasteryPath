34b65f9d0d0e55eecd53df4b5ded8689186f6e88
Abhinav Kumar <abhinavkumar93043@example.com>
2024-09-07 11:25:50 +0530

    hiding TOC when clicking on the liks and added styles

--- clients/src/pages/Blog/Toc.jsx
+++ clients/src/pages/Blog/Toc.jsx
@@ -58,25 +58,30 @@ const Toc = ({ content, className }) => {
         <>
             {(toc.length > 1) && <div className={`sticky top-20 md:mr-6 h-[80vh] z-[100] ${className}`}>
                 <div className={`flex items-center h-full dark:text-slate-100 absolute ${showTOC ? '-left-6 sm:-left-14' : '-left-6 sm:-left-16 md:-left-8'} md:relative`}>
-                    <div className={`${showTOC ? 'w-64 border-r px-5' : ''} transition-all duration-300 w-0 md:border-r md:pr-5 py-5 md:w-full overflow-x-hidden overflow-y-auto h-full backdrop-blur-xl `}>
-                        <h1 className='text-2xl underline mb-4'>Table of Contents</h1>
-                        <ul className='space-y-1'>
-                            {toc.map((item, index) => {
-                                const indentLevel = item.level - item.baseLevel;
-                                return (
-                                    <li
-                                        key={index}
-                                        className={`ml-${indentLevel * 4} flex transition-all duration-300 ${currentId === item.id ? 'text-sky-500' : 'hover:text-sky-300'}`}
-                                        style={{ marginLeft: `${indentLevel * 1.5}rem` }}
-                                        onClick={() => setCurrentId(item.id)}
-                                    >
-                                        <strong className='mr-2'> -</strong> <a href={`#${item.id}`} className='text-sm hover:underline'>{item.text}</a>
-                                    </li>
-                                )
-                            })}
-                        </ul>
+                    <div className={`${showTOC ? 'w-64 border-r px-5' : ''} transition-all duration-300 w-0 md:border-r md:pr-5 py-5 md:w-full overflow-x-hidden overflow-y-auto h-full backdrop-blur-xl bg-slate-300/60 dark:bg-primary/60  `}>
+                        <div className='w-56'>
+                            <h1 className='text-2xl underline mb-4'>Table of Contents</h1>
+                            <ul className='space-y-1'>
+                                {toc.map((item, index) => {
+                                    const indentLevel = item.level - item.baseLevel;
+                                    return (
+                                        <li
+                                            key={index}
+                                            className={`ml-${indentLevel * 4} flex transition-all duration-300 ${currentId === item.id ? 'text-sky-600 dark:text-sky-400' : 'hover:text-sky-800 dark:hover:text-sky-300'}`}
+                                            style={{ marginLeft: `${indentLevel * 1}rem` }}
+                                            onClick={() => {
+                                                setCurrentId(item.id);
+                                                setShowTOC(false);
+                                            }}
+                                        >
+                                            <strong className='mr-2'>-</strong> <a href={`#${item.id}`} className='text-sm hover:underline'>{item.text}</a>
+                                        </li>
+                                    )
+                                })}
+                            </ul>
+                        </div>
                     </div>
-                    <button className='px-1 py-2 md:hidden bg-slate-600 rounded-r-md' onClick={() => setShowTOC(prev => !prev)}><div className=''>{'>'}</div></button>
+                    <button className='px-1 py-2 md:hidden bg-slate-600 text-slate-100 rounded-r-md' onClick={() => setShowTOC(prev => !prev)} title={showTOC ? "Hide TOC" : "Show TOC"}>{showTOC ? '<' : '>'}</button>
                 </div>
             </div>}
         </>
