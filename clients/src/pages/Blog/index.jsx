b2e64983d476894d8f2ab3980c2129ba0c0106e5
Abhinav Kumar <abhinavkumar93043@example.com>
2024-09-09 17:46:20 +0530

    setting the blog search header to the top

--- clients/src/pages/Blog/index.jsx
+++ clients/src/pages/Blog/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useUserAuthentication } from '../../context/userContext';
 import { Link } from 'react-router-dom';
 import BlogItem from './BlogItem';
@@ -12,6 +12,7 @@ const apiUrl = import.meta.env.VITE_API_URL;
 const Blog = () => {
   const { isAuthenticatedUser } = useUserAuthentication();
   const { blogs, hasMore, loading, loaderDiv, perPage, search, setSearch, sort, setSort, resetBlogSummaries, errorMessage } = useBlogSummaries(`${apiUrl}/blog`);
+  const [headerScrolledToTop, setHeaderScrolledToTop] = useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
     resetBlogSummaries();
@@ -25,9 +26,28 @@ const Blog = () => {
     return <ErrorMessage heading='Unable to fetch blog' message={errorMessage} action={resetBlogSummaries} />
   }
 
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY + 60;
+
+    const element = document.getElementById('blog-header');
+    if (element && element.offsetTop <= scrollPosition) {
+      setHeaderScrolledToTop(true);
+    } else {
+      setHeaderScrolledToTop(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div>
-      <div className='mb-5 h-16 flex justify-between items-center px-4 sticky top-20 z-[100] rounded-md bg-primary'>
+      <div id='blog-header' className={`mb-5 h-16 flex justify-between items-center px-4 sticky top-2 ${headerScrolledToTop ? 'z-[1001]' : 'z-[100]'} rounded-md bg-primary/60 backdrop-blur-md`}>
         {isAuthenticatedUser && <Link to='/create_blog' className=''>
           <div className="h-10 rounded-xl font-bold bg-blue-gradient flex items-center px-4 text-sm">
             <ion-icon name="add-outline" className='font-bold'></ion-icon> Write Blog
